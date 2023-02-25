715fb0ffff34659315e9577981263f37a7d92096
sergeybaranov <se.bar2300@mail.ru>
2023-02-25 12:20:36 +0300

    Initial Commit

--- objects.py
+++ objects.py
@@ -28,24 +28,24 @@ class Background():
 
 
 class Dino():
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x, self.base = x, y
 
         self.run_list = []
         self.duck_list = []
 
-        for i in range(1,4):
+        for i in range(1, 4):
             dino_img = pygame.image.load(f'Dino/Dino/{i}.png')
-            dino_img = pygame.transform.scale(dino_img, (52,58))
+            dino_img = pygame.transform.scale(dino_img, (52, 58))
             self.run_list.append(dino_img)
 
-        for i in range (4,6):
+        for i in range(4, 6):
             dino_img = pygame.image.load(f'Dino/Dino/{i}.png')
-            dino_img = pygame.transform.scale(dino_img, (70,38))
+            dino_img = pygame.transform.scale(dino_img, (70, 38))
             self.duck_list.append(dino_img)
 
         self.dead_image = pygame.image.load(f'Dino/Dino/8.png')
-        self.dead_image = pygame.transform.scale(self.dead_image,(52,58))
+        self.dead_image = pygame.transform.scale(self.dead_image, (52, 58))
 
         self.reset()
 
@@ -88,7 +88,72 @@ class Dino():
                     self.rect.x = self.x = self.x
                     self.rect.bottom = self.base
                     self.counter = 0
+            elif self.isJumping:
+                self.index = 0
+                self.counter = 0
+                self.image = self.run_list[self.index]
+            else:
+                self.counter += 1
+                if self.counter >= 4:
+                    self.index = (self.index + 1) % len(self.run_list)
+                    self.image = self.run_list[self.index]
+                    self.rect = self.image.get_rect()
+                    self.rect.x = self.x
+                    self.rect.bottom = self.base
+                    self.counter = 0
+
+            self.mask = pygame.mask.from_surface(self.image)
+
+        else:
+            self.image = self.dead_image
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
 
 
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super(Cactus, self).__init__()
+
+        self.image_list = []
+        for i in range(5):
+            scale = 0.65
+            img = pygame.image.load(f'Dino/Cactus/{i + 1}.png')
+            w, h = img.get_size()
+            img = pygame.transform.scale(img, (int(w * scale), int(h * scale)))
+            self.image_list.append(img)
+
+        self.image = self.image_list[type - 1]
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH + 10
+        self.rect.bottom = 165
+
+    def update(self, speed, dino):
+        if dino.alive:
+            self.rect.x -= speed
+            if self.rect.right <= 0:
+                self.kill()
+
+            self.mask = pygame.mask.from_surface(self.image)
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
 
 
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Cloud, self).__init__()
+        self.image = pygame.image.load(f'Dino/cloud.png')
+        self.image = pygame.transform.scale(self.image, (60, 18))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, speed, dino):
+        if dino.alive:
+            self.rect.x -= speed
+            if self.rect.right <= 0:
+                self.kill()
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
