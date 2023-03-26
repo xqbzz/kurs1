2f66d9045523998d4225cd80424230e819b774ad
sergeybaranov <se.bar2300@mail.ru>
2023-03-26 18:36:39 +0300

    next coding

--- main.py
+++ main.py
@@ -9,3 +9,39 @@ win = pygame.display.set_made(SCREEN, pygame.NOFRAME)
 
 clock = pygame.time.Clock()
 FPS = 60
+
+#ЦВЕТ
+
+WHITE = (225,225,225)
+BLACK = ( 0, 0, 0)
+GRAY = (32, 33, 36)
+GREENYELLOW = (173,255,47)
+GREEN = ( 0, 100, 0)
+#......
+#ВЫВОД КАРТИНОК
+start_img = pygame.image.load('Dino/start_img.png')
+start_img = pygame.transform.scale(start_img, (60,64))
+
+game_over_img = pygame.image.load('Dino/game_over.png')
+game_over_img = pygame.transform.scale(game_over_img, (200,36))
+
+replay_img = pygame.image.load('Dino/ replay.png')
+replay_img = pygame.transform.scale(replay_img, (40, 36))
+replay_rect = replay_img.get_rect()
+replay_rect.x = WIDTH // 2 -30
+replay_rect.y = 100
+
+#МУЗЫКА(СДЕЛАТЬ)
+#
+#
+#
+#
+
+#ОБЪЕКТЫ
+ground = Background()
+dino = Dino(50, 160)
+
+cactus_group = pygame.sprite.Group()
+ptera_group = pygame.sprite.Group()
+cloud_group = pygame.sprite.Group()
+stars_group = pygame.sprite.Group()
