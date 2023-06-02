f330c691ff2b1c74096d89a02ecee310dac4de4f
sergeybaranov <se.bar2300@mail.ru>
2023-06-02 23:25:19 +0300

    Done(почти почти)

--- main.py
+++ main.py
@@ -10,66 +10,72 @@ win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
 clock = pygame.time.Clock()
 FPS = 60
 
-#ЦВЕТ
+# COLORS *********************************************************************
 
 WHITE = (225,225,225)
-BLACK = ( 0, 0, 0)
+BLACK = (0, 0, 0)
 GRAY = (32, 33, 36)
-GREENYELLOW = (173,255,47)
-GREEN = ( 0, 100, 0)
-#......
-#ВЫВОД КАРТИНОК
+
+# IMAGES *********************************************************************
+
 start_img = pygame.image.load('Dino/start_img.png')
-start_img = pygame.transform.scale(start_img, (80,80))
+start_img = pygame.transform.scale(start_img, (400, 400))
+start_rect = start_img.get_rect()
+start_rect.center = (WIDTH // 2, HEIGHT // 2)
 
 game_over_img = pygame.image.load('Dino/game_over.png')
-game_over_img = pygame.transform.scale(game_over_img, (200,36))
+game_over_img = pygame.transform.scale(game_over_img, (200, 36))
 
 replay_img = pygame.image.load('Dino/replay.png')
 replay_img = pygame.transform.scale(replay_img, (40, 36))
 replay_rect = replay_img.get_rect()
-replay_rect.x = WIDTH // 2 -30
+replay_rect.x = WIDTH // 2 - 20
 replay_rect.y = 100
 
 numbers_img = pygame.image.load('Dino/numbers.png')
-numbers_img = pygame.transform.scale(numbers_img,(120,12))
+numbers_img = pygame.transform.scale(numbers_img, (120, 12))
 
-#МУЗЫКА(СДЕЛАТЬ)
-#
-#
-#
-#
+# SOUNDS *********************************************************************
 
-#ОБЪЕКТЫ
+#jump_fx = pygame.mixer.Sound('Sounds/jump.wav')
+#die_fx = pygame.mixer.Sound('Sounds/die.wav')
+#checkpoint_fx = pygame.mixer.Sound('Sounds/checkPoint.wav')
+
+# OBJECTS & GROUPS ***********************************************************
+
 ground = Background()
-dino = Dino(100, 100)
+dino = Dino(50, 160)
 
 cactus_group = pygame.sprite.Group()
 ptera_group = pygame.sprite.Group()
 cloud_group = pygame.sprite.Group()
 stars_group = pygame.sprite.Group()
 
-#FUNC
+# FUNCTIONS ******************************************************************
 
 def reset():
-    global counter, SPEED, score, high_score
+	global counter, SPEED, score, high_score
 
-    if score and score >= high_score:
-        high_score = score
+	if score and score >= high_score:
+		high_score = score
 
-    counter = 0
-    SPEED = 5
-    score = 0
+	counter = 0
+	SPEED = 5
+	score = 0
 
-    cactus_group.empty()
-    ptera_group.empty()
-    cloud_group.empty()
-    stars_group.empty()
+	cactus_group.empty()
+	ptera_group.empty()
+	cloud_group.empty()
+	stars_group.empty()
 
-    dino.reset()
+	dino.reset()
 
+keys = []
+GODMODE = False
+DAYMODE = False
+LYAGAMI = False
 counter = 0
-enemy_time = 100
+enemy_time = 80
 cloud_time = 500
 stars_time = 175
 
@@ -85,107 +91,141 @@ mouse_pos = (-1, -1)
 
 running = True
 while running:
-    jump = False
-    duck = False  
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
-                running = False
-        
-            if event.key == pygame.K_SPACE: 
-                if start_page:
-                    start_page = False
-                elif dino.alive:
-                    jump = True
-                    #SOUND
-                else:
-                    reset()
-                
-            if event.key == pygame.K_DOWN:     
-                duck = True
+	jump = False
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
-                jump = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+				running = False
 
-            if event.key == pygame.K_DOWN:
-                duck = False
+			if event.key == pygame.K_SPACE:
+				if start_page:
+					start_page = False
+				elif dino.alive:
+					jump = True
+					#jump_fx.play()
+				else:
+					reset()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = event.pos
+			if event.key == pygame.K_UP:
+				jump = True
+				#jump_fx.play()
 
-        if event.type == pygame.MOUSEBUTTONUP:
-            mouse_pos = (-1, -1)
+			if event.key == pygame.K_DOWN:
+				duck = True
 
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+				jump = False
 
-    if start_page:
-        win.blit(start_img, (50, 100))
-    else:
-        if dino.alive:
-            counter +=1
-            if counter % int(enemy_time) == 0:
-                if random.randint(1, 10) == 5:
-                    y = random.choice([85, 130])
-                    ptera = Ptera(WIDTH, y)
-                    ptera_group.add(ptera)
-                else:
-                    type = random.randint(1,4)
-                    cactus = Cactus(type)
-                    cactus_group.add(cactus)
+			if event.key == pygame.K_DOWN:
+				duck = False
 
-            if counter % cloud_time == 0:
-                y = random.randint(100,100)
-                cloud = Cloud(WIDTH, y)
-                cloud_group.add(cloud)
-            
-            if counter % stars_time == 0:
-                type = random.randint(1,3)
-                y = random.randint(40, 100)
-                star = Star(WIDTH, y, type)
-                stars_group.add(star)
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			mouse_pos = event.pos
 
-            if counter % 100 == 0:
-                SPEED += 0.1
-                enemy_time -= 0.5
+		if event.type == pygame.MOUSEBUTTONUP:
+			mouse_pos = (-1, -1)
 
-            if counter % 5 == 0:
-                score +=1
+	if start_page:
+		win.blit(start_img, (100, 0))
+	else:
+		if dino.alive:
+			counter += 1
+			if counter % int(enemy_time) == 0:
+				if random.randint(1, 10) == 5:
+					y = random.choice([85, 130])
+					ptera = Ptera(WIDTH, y)
+					ptera_group.add(ptera)
+				else:
+					type = random.randint(1, 4)
+					cactus = Cactus(type)
+					cactus_group.add(cactus)
 
-            '''if score and score % 100 == 0:
-                #MUSIC'''
+			if counter % cloud_time == 0:
+				y = random.randint(40, 100)
+				cloud = Cloud(WIDTH, y)
+				cloud_group.add(cloud)
 
-        ground.update(SPEED)
-        ground.draw(win)
-        cloud_group.update(SPEED-3, dino)
-        cloud_group.draw(win)
-        stars_group.update(SPEED-3,dino)
-        stars_group.draw(win)
-        cactus_group.update(SPEED, dino)
-        cactus_group.draw(win)
-        ptera_group.update(SPEED-2, dino)
-        ptera_group.draw(win)
-        dino.update(jump, duck)
-        dino.draw
+			if counter % stars_time == 0:
+				type = random.randint(1, 3)
+				y = random.randint(40, 100)
+				star = Star(WIDTH, y, type)
+				stars_group.add(star)
 
-        string_score = str(score).zfill(5)
-        for i, num in enumerate(string_score):
-            win.blit(numbers_img,(520+11*i,10),(10*int(num), 0, 10, 12))
+			if counter % 100 == 0:
+				SPEED += 0.1
+				enemy_time -= 0.5
 
-        if high_score:
-            win.blit(numbers_img,(425,10),(100,0,20,12))
-            string_score = f'{high_score}'.zfill(5)
-            for i, num in enumerate(string_score):
-                win.blit(numbers_img, (455+11*i,10))
-        
-        if not dino.alive:
-            win.blit(game_over_img,(WIDTH//2-100,55))
-            win.blit(replay_img, replay_rect)
+			if counter % 5 == 0:
+				score += 1
 
-            if replay_rect.collidedict(mouse_pos):
-                reset()
+			#if score and score % 100 == 0:
+				#checkpoint_fx.play()
 
-    pygame.draw.rect(win,WHITE,(0,0,WIDTH,HEIGHT),4)
-    clock.tick(FPS)
-    pygame.display.update()
+			if not GODMODE:
+				for cactus in cactus_group:
+					if LYAGAMI:
+						dx = cactus.rect.x - dino.rect.x
+						if 0 <= dx <= (70 + (score//100)):
+							jump = True
+
+					if pygame.sprite.collide_mask(dino, cactus):
+						SPEED = 0
+						dino.alive = False
+						#die_fx.play()
+
+				for cactus in ptera_group:
+					if LYAGAMI:
+						dx = ptera.rect.x - dino.rect.x
+						if 0 <= dx <= 70:
+							if dino.rect.top <= ptera.rect.top:
+								jump = True
+							else:
+								duck = True
+						else:
+							duck = False
+
+					if pygame.sprite.collide_mask(dino, ptera):
+						SPEED = 0
+						dino.alive = False
+						#die_fx.play()
+
+		ground.update(SPEED)
+		ground.draw(win)
+		cloud_group.update(SPEED-3, dino)
+		cloud_group.draw(win)
+		stars_group.update(SPEED-3, dino)
+		stars_group.draw(win)
+		cactus_group.update(SPEED, dino)
+		cactus_group.draw(win)
+		ptera_group.update(SPEED-1, dino)
+		ptera_group.draw(win)
+		dino.update(jump, duck)
+		dino.draw(win)
+
+		string_score = str(score).zfill(5)
+		for i, num in enumerate(string_score):
+			win.blit(numbers_img, (520+11*i, 10), (10*int(num), 0, 10, 12))
+
+		if high_score:
+			win.blit(numbers_img, (425, 10), (100, 0, 20, 12))
+			string_score = f'{high_score}'.zfill(5)
+			for i, num in enumerate(string_score):
+				win.blit(numbers_img, (455+11*i, 10), (10*int(num), 0, 10, 12))
+
+		if not dino.alive:
+			win.blit(game_over_img, (WIDTH//2-100, 55))
+			win.blit(replay_img, replay_rect)
+
+			if replay_rect.collidepoint(mouse_pos):
+				reset()
+
+	pygame.draw.rect(win, WHITE, (0, 0, WIDTH, HEIGHT), 4)
+	clock.tick(FPS)
+	pygame.display.update()
+	
 
 pygame.quit()
--- objects.py
+++ objects.py
@@ -1,8 +1,6 @@
 import pygame
 
-WIDTH = 600
-HEIGHT = 400
-SCREEN = WIDTH, HEIGHT
+SCREEN = WIDTH, HEIGHT = (600, 400)
 
 class Background():
     def __init__(self):
@@ -28,192 +26,194 @@ class Background():
     def draw(self, win):
         win.blit(pygame.transform.scale(self.image, (self.scaled_width, self.scaled_height)), (self.x1, self.y))
         win.blit(pygame.transform.scale(self.image, (self.scaled_width, self.scaled_height)), (self.x2, self.y))
+
+
 class Dino():
-    def __init__(self, x, y):
-        self.x, self.y, self.base = x, y, y
+	def __init__(self, x, y):
+		self.x, self.base = x, 385
 
-        self.run_list = []
-        self.duck_list = []
+		self.run_list = []
+		self.duck_list = []
 
-        for i in range(1, 4):
-            dino_img = pygame.image.load(f'Dino/Dino/{i}.png')
-            dino_img = pygame.transform.scale(dino_img, (44, 47))
-            self.run_list.append(dino_img)
+		for i in range(1, 4):
+			img = pygame.image.load(f'DIno/Dino/{i}.png')
+			img = pygame.transform.scale(img, (52, 58))
+			self.run_list.append(img)
 
-        for i in range(4, 6):
-            dino_img = pygame.image.load(f'Dino/Dino/{i}.png')
-            dino_img = pygame.transform.scale(dino_img, (62, 36))
-            self.duck_list.append(dino_img)
+		for i in range(4, 6):
+			img = pygame.image.load(f'Dino/Dino/{i}.png')
+			img = pygame.transform.scale(img, (70, 38))
+			self.duck_list.append(img)
 
-        self.dead_image = pygame.image.load(f'Dino/Dino/8.png')
-        self.dead_image = pygame.transform.scale(self.dead_image, (44, 47))
+		self.dead_image = pygame.image.load(f'Dino/Dino/8.png')
+		self.dead_image = pygame.transform.scale(self.dead_image, (52,58))
 
-        self.reset()
+		self.reset()
 
-        self.vel = 0
-        self.gravity = 1
-        self.jumpHeight = 15
-        self.isJumping = False
+		self.vel = 0
+		self.gravity = 1
+		self.jumpHeight = 15
+		self.isJumping = False
 
-    def reset(self):
-        self.index = 0
-        self.image = self.run_list[self.index]
-        self.rect = self.image.get_rect()
-        self.rect.x = self.x
-        self.rect.bottom = self.base
+	def reset(self):
+		self.index = 0
+		self.image = self.run_list[self.index]
+		self.rect = self.image.get_rect()
+		self.rect.x = self.x
+		self.rect.bottom = self.base
 
-        self.alive = True
-        self.counter = 0
+		self.alive = True
+		self.counter = 0
 
-    def update(self, jump, duck):
-        if self.alive:
-            if not self.isJumping and jump:
-                self.vel = -self.jumpHeight
-                self.isJumping = True
+	def update(self, jump, duck):
+		if self.alive:
+			if not self.isJumping and jump:
+				self.vel = -self.jumpHeight
+				self.isJumping = True
 
-            self.vel += self.gravity
-            if self.vel >= self.jumpHeight:
-                self.vel = self.jumpHeight
+			self.vel += self.gravity
+			if self.vel >= self.jumpHeight:
+				self.vel = self.jumpHeight
 
-            self.rect.y += self.vel
-            if self.rect.bottom > self.base:
-                self.rect.bottom = self.base
-                self.isJumping = False
+			self.rect.y += self.vel
+			if self.rect.bottom > self.base:
+				self.rect.bottom = self.base
+				self.isJumping = False
 
-            if duck:
-                self.counter += 1
-                if self.counter >= 6:
-                    self.index = (self.index + 1) % len(self.duck_list)
-                    self.image = self.duck_list[self.index]
-                    self.rect = self.image.get_rect()
-                    self.rect.x = self.x = self.x
-                    self.rect.bottom = self.base
-                    self.counter = 0
-            elif self.isJumping:
-                self.index = 0
-                self.counter = 0
-                self.image = self.run_list[self.index]
-            else:
-                self.counter += 1
-                if self.counter >= 4:
-                    self.index = (self.index + 1) % len(self.run_list)
-                    self.image = self.run_list[self.index]
-                    self.rect = self.image.get_rect()
-                    self.rect.x = self.x
-                    self.rect.bottom = self.base
-                    self.counter = 0
+			if duck:
+				self.counter += 1
+				if self.counter >= 6:
+					self.index = (self.index + 1) % len(self.duck_list)
+					self.image = self.duck_list[self.index]
+					self.rect = self.image.get_rect()
+					self.rect.x = self.x
+					self.rect.bottom = self.base
+					self.counter = 0
 
-            self.mask = pygame.mask.from_surface(self.image)
+			elif self.isJumping:
+				self.index = 0
+				self.counter = 0
+				self.image = self.run_list[self.index]
+			else:
+				self.counter += 1
+				if self.counter >= 4:
+					self.index = (self.index + 1) % len(self.run_list)
+					self.image = self.run_list[self.index]
+					self.rect = self.image.get_rect()
+					self.rect.x = self.x
+					self.rect.bottom = self.base
+					self.counter = 0
 
-        else:
-            self.image = self.dead_image
+			self.mask = pygame.mask.from_surface(self.image)
 
-    def draw(self, win):
-        win.blit(self.image, self.rect)
+		else:
+			self.image = self.dead_image
+
+	def draw(self, win):
+		win.blit(self.image, self.rect)
 
 
 class Cactus(pygame.sprite.Sprite):
-    def __init__(self, type):
-        super(Cactus, self).__init__()
+	def __init__(self, type):
+		super(Cactus, self).__init__()
 
-        self.image_list = []
-        for i in range(5):
-            scale = 0.7
-            img = pygame.image.load(f'Dino/Cactus/{i + 1}.png')
-            w, h = img.get_size()
-            img = pygame.transform.scale(img, (int(w * scale), int(h * scale)))
-            self.image_list.append(img)
+		self.image_list = []
+		for i in range(8):
+			scale = 0.75
+			img = pygame.image.load(f'DIno/Cactus/{i+1}.png')
+			w, h = img.get_size()
+			img = pygame.transform.scale(img, (int(w*scale), int(h*scale)))
+			self.image_list.append(img)
 
-        self.image = self.image_list[type - 1]
-        self.rect = self.image.get_rect()
-        self.rect.x = WIDTH + 10
-        self.rect.bottom = 165
+		self.image = self.image_list[type-1]
+		self.rect = self.image.get_rect()
+		self.rect.x = WIDTH + 10
+		self.rect.bottom = 385
 
-    def update(self, speed, dino):
-        if dino.alive:
-            self.rect.x -= speed
-            if self.rect.right <= 0:
-                self.kill()
+	def update(self, speed, dino):
+		if dino.alive:
+			self.rect.x -= speed
+			if self.rect.right <= 0:
+				self.kill()
 
-            self.mask = pygame.mask.from_surface(self.image)
+			self.mask = pygame.mask.from_surface(self.image)
 
-    def draw(self, win):
-        win.blit(self.image, self.rect)
-
-
-class Cloud(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super(Cloud, self).__init__()
-        self.image = pygame.image.load(f'Dino/cloud.png')
-        self.image = pygame.transform.scale(self.image, (60,20))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-    def update(self, speed, dino):
-        if dino.alive:
-            self.rect.x -= speed
-            if self.rect.right <= 0:
-                self.kill()
-
-    def draw(self, win):
-        win.blit(self.image, self.rect)
+	def draw(self, win):
+		win.blit(self.image, self.rect)
 
 
 class Ptera(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super(Ptera, self).__init__()
+	def __init__(self, x, y):
+		super(Ptera, self).__init__()
 
-        self.image_list = []
-        for i in range(2):
-            scale = 0.65
-            img = pygame.image.load(f'Dino/Ptera/{i+1}.png')
-            w, h = img.get_size()
-            img = pygame.transform.scale(img, (int(w * scale), int(h * scale)))
-            self.image_list.append(img)
+		self.image_list = []
+		for i in range(2):
+			scale = 0.65
+			img = pygame.image.load(f'DIno/Ptera/{i+1}.png')
+			w, h = img.get_size()
+			img = pygame.transform.scale(img, (int(w*scale), int(h*scale)))
+			self.image_list.append(img)
 
-        self.index = 0
-        self.image = self.image_list[self.index]
-        self.rect = self.image.get_rect(center=(x, y))
+		self.index = 0
+		self.image = self.image_list[self.index]
+		self.rect = self.image.get_rect(center=(x, y))
 
-        self.counter = 0
+		self.counter = 0
 
-    def update(self, speed, dino):
-        if dino.alive:
-            self.rect.x -= speed
-            if self.rect.right <= 0:
-                self.kill()
+	def update(self, speed, dino):
+		if dino.alive:
+			self.rect.x -= speed
+			if self.rect.right <= 0:
+				self.kill()
 
-            self.counter += 1
-            if self.counter >= 6:
-                self.index = (self.index + 1) % len(self.image_list)
-                self.image = self.image_list[self.index]
-                self.counter = 0
+			self.counter += 1
+			if self.counter >= 6:
+				self.index = (self.index + 1) % len(self.image_list)
+				self.image = self.image_list[self.index]
+				self.counter = 0
 
-            self.mask = pygame.mask.from_surface(self.image)
+			self.mask = pygame.mask.from_surface(self.image)
 
-    def draw(self, win):
-        win.blit(self.image, self.rect)
+	def draw(self, win):
+		win.blit(self.image, self.rect)
 
+
+class Cloud(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		super(Cloud, self).__init__()
+		self.image = pygame.image.load(f'DIno/cloud.png')
+		self.image = pygame.transform.scale(self.image, (60, 18))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+	def update(self, speed, dino):
+		if dino.alive:
+			self.rect.x -= speed
+			if self.rect.right <= 0:
+				self.kill()
+
+	def draw(self, win):
+		win.blit(self.image, self.rect)
 
 class Star(pygame.sprite.Sprite):
-    def __init__(self, x, y, type):
-        super(Star, self).__init__()
-        image = pygame.image.load(f'Dino/stars.png')
-        self.image_list = []
-        for i in range(3):
-            img = image.subsurface((0, 20 * (i), 18, 18))
-            self.image_list.append(img)
-        self.image = self.image_list[type - 1]
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+	def __init__(self, x, y, type):
+		super(Star, self).__init__()
+		image = pygame.image.load(f'Dino/stars.png')
+		self.image_list = []
+		for i in range(3):
+			img = image.subsurface((0, 20*(i), 18, 18))
+			self.image_list.append(img)
+		self.image = self.image_list[type-1]
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
 
-    def update(self, speed, dino):
-        if dino.alive:
-            self.rect.x -= speed
-            if self.rect.right <= 0:
-                self.kill()
+	def update(self, speed, dino):
+		if dino.alive:
+			self.rect.x -= speed
+			if self.rect.right <= 0:
+				self.kill()
 
-    def draw(self, win):
-        win.blit(self.image, self.rect)
+	def draw(self, win):
+		win.blit(self.image, self.rect)
