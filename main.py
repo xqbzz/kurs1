9cc9062255b0eb86c9e68ff6bc086dc2dc55eaff
sergeybaranov <se.bar2300@mail.ru>
2023-06-03 02:32:26 +0300

    Done(Music)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 import pygame
+import os
+import av
+import imageio
+import datetime
 
 from objects import Background, Dino, Cactus, Cloud, Ptera, Star
 
@@ -9,7 +13,11 @@ win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
 
 clock = pygame.time.Clock()
 FPS = 60
-
+record = False
+output_folder = "recordings"  # Папка для сохранения записей(название любое)
+video_writer = None
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 # COLORS *********************************************************************
 
 WHITE = (225,225,225)
@@ -35,6 +43,12 @@ replay_rect.y = 100
 numbers_img = pygame.image.load('Dino/numbers.png')
 numbers_img = pygame.transform.scale(numbers_img, (120, 12))
 
+button_nornal_img = pygame.image.load('Dino/Button/Normal.png')
+button_hard_img = pygame.image.load('Dino/Button/Hard.png')
+button_ultra_img = pygame.image.load('Dino/Button/Ultra.png')
+button_selected_normal_img = pygame.image.load('Dino/Button/NormalRED.png')
+button_selected_hard_img = pygame.image.load('Dino/Button/HardRED.png')
+button_selected_ultra_img = pygame.image.load('Dino/Button/UltraRED.png')
 # SOUNDS *********************************************************************
 
 #jump_fx = pygame.mixer.Sound('Sounds/jump.wav')
@@ -51,7 +65,28 @@ ptera_group = pygame.sprite.Group()
 cloud_group = pygame.sprite.Group()
 stars_group = pygame.sprite.Group()
 
+container = av.open(os.path.join(output_folder, "game_record.mp4"), mode='w')
+video_stream = container.add_stream('libx264', rate=60)
+video_stream.width = WIDTH
+video_stream.height = HEIGHT
+
+button_normal_pos = (100, 100)
+button_hard_pos = (200, 100)
+button_ultra_pos = (300, 100)
+
 # FUNCTIONS ******************************************************************
+def start_record():
+    global record, video_writer
+    record = True
+    screen_size = (WIDTH, HEIGHT)
+    current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")  # Генерация строки с текущей датой и временем
+    video_filename = os.path.join(output_folder, f"game_record_{current_datetime}.mp4")
+    video_writer = imageio.get_writer(video_filename, fps=60)
+
+def stop_record():
+    global record, video_writer
+    record = False
+    video_writer.close()
 
 def reset():
 	global counter, SPEED, score, high_score
@@ -90,142 +125,161 @@ start_page = True
 mouse_pos = (-1, -1)
 
 running = True
+paused = False
 while running:
-	jump = False
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+    jump = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
-				running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                running = False
 
-			if event.key == pygame.K_SPACE:
-				if start_page:
-					start_page = False
-				elif dino.alive:
-					jump = True
-					#jump_fx.play()
-				else:
-					reset()
+            if event.key == pygame.K_r:
+                if not record:
+                    start_record()
+                else:
+                    stop_record()
 
-			if event.key == pygame.K_UP:
-				jump = True
-				#jump_fx.play()
+            if event.key == pygame.K_p:
+                paused = not paused
 
-			if event.key == pygame.K_DOWN:
-				duck = True
+            if not paused:
+                if event.key == pygame.K_SPACE:
+                    if start_page:
+                        start_page = False
+                    elif dino.alive:
+                        jump = True
+                        # jump_fx.play()
+                    else:
+                        reset()
 
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
-				jump = False
+                if event.key == pygame.K_UP:
+                    jump = True
+                    # jump_fx.play()
 
-			if event.key == pygame.K_DOWN:
-				duck = False
+                if event.key == pygame.K_DOWN:
+                    duck = True
 
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			mouse_pos = event.pos
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                jump = False
 
-		if event.type == pygame.MOUSEBUTTONUP:
-			mouse_pos = (-1, -1)
+            if event.key == pygame.K_DOWN:
+                duck = False
 
-	if start_page:
-		win.blit(start_img, (100, 0))
-	else:
-		if dino.alive:
-			counter += 1
-			if counter % int(enemy_time) == 0:
-				if random.randint(1, 10) == 5:
-					y = random.choice([85, 130])
-					ptera = Ptera(WIDTH, y)
-					ptera_group.add(ptera)
-				else:
-					type = random.randint(1, 4)
-					cactus = Cactus(type)
-					cactus_group.add(cactus)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
 
-			if counter % cloud_time == 0:
-				y = random.randint(40, 100)
-				cloud = Cloud(WIDTH, y)
-				cloud_group.add(cloud)
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_pos = (-1, -1)
 
-			if counter % stars_time == 0:
-				type = random.randint(1, 3)
-				y = random.randint(40, 100)
-				star = Star(WIDTH, y, type)
-				stars_group.add(star)
+    if start_page:
+        win.blit(start_img, (100, 0))
+    else:
+        if dino.alive and not paused:
+            counter += 1
+            if counter % int(enemy_time) == 0:
+                if random.randint(1, 10) == 5:
+                    y = random.choice([85, 130])
+                    ptera = Ptera(WIDTH, y)
+                    ptera_group.add(ptera)
+                else:
+                    type = random.randint(1, 4)
+                    cactus = Cactus(type)
+                    cactus_group.add(cactus)
 
-			if counter % 100 == 0:
-				SPEED += 0.1
-				enemy_time -= 0.5
+            if counter % cloud_time == 0:
+                y = random.randint(40, 100)
+                cloud = Cloud(WIDTH, y)
+                cloud_group.add(cloud)
 
-			if counter % 5 == 0:
-				score += 1
+            if counter % stars_time == 0:
+                type = random.randint(1, 3)
+                y = random.randint(40, 100)
+                star = Star(WIDTH, y, type)
+                stars_group.add(star)
 
-			#if score and score % 100 == 0:
-				#checkpoint_fx.play()
+            if counter % 100 == 0:
+                SPEED += 0.1
+                enemy_time -= 0.5
 
-			if not GODMODE:
-				for cactus in cactus_group:
-					if LYAGAMI:
-						dx = cactus.rect.x - dino.rect.x
-						if 0 <= dx <= (70 + (score//100)):
-							jump = True
+            if counter % 5 == 0:
+                score += 1
 
-					if pygame.sprite.collide_mask(dino, cactus):
-						SPEED = 0
-						dino.alive = False
-						#die_fx.play()
+            if not GODMODE:
+                for cactus in cactus_group:
+                    if LYAGAMI:
+                        dx = cactus.rect.x - dino.rect.x
+                        if 0 <= dx <= (70 + (score // 100)):
+                            jump = True
 
-				for cactus in ptera_group:
-					if LYAGAMI:
-						dx = ptera.rect.x - dino.rect.x
-						if 0 <= dx <= 70:
-							if dino.rect.top <= ptera.rect.top:
-								jump = True
-							else:
-								duck = True
-						else:
-							duck = False
+                    if pygame.sprite.collide_mask(dino, cactus):
+                        SPEED = 0
+                        dino.alive = False
+                        # die_fx.play()
 
-					if pygame.sprite.collide_mask(dino, ptera):
-						SPEED = 0
-						dino.alive = False
-						#die_fx.play()
+                for cactus in ptera_group:
+                    if LYAGAMI:
+                        dx = ptera.rect.x - dino.rect.x
+                        if 0 <= dx <= 70:
+                            if dino.rect.top <= ptera.rect.top:
+                                jump = True
+                            else:
+                                duck = True
+                        else:
+                            duck = False
 
-		ground.update(SPEED)
-		ground.draw(win)
-		cloud_group.update(SPEED-3, dino)
-		cloud_group.draw(win)
-		stars_group.update(SPEED-3, dino)
-		stars_group.draw(win)
-		cactus_group.update(SPEED, dino)
-		cactus_group.draw(win)
-		ptera_group.update(SPEED-1, dino)
-		ptera_group.draw(win)
-		dino.update(jump, duck)
-		dino.draw(win)
+                    if pygame.sprite.collide_mask(dino, ptera):
+                        SPEED = 0
+                        dino.alive = False
+                        # die_fx.play()
 
-		string_score = str(score).zfill(5)
-		for i, num in enumerate(string_score):
-			win.blit(numbers_img, (520+11*i, 10), (10*int(num), 0, 10, 12))
+        if paused:
+            win.blit(start_img, (100, 0))  # Отображение изображения start_img
+            pygame.display.update()
+            continue  # Пропуск обновления игры в режиме паузы
 
-		if high_score:
-			win.blit(numbers_img, (425, 10), (100, 0, 20, 12))
-			string_score = f'{high_score}'.zfill(5)
-			for i, num in enumerate(string_score):
-				win.blit(numbers_img, (455+11*i, 10), (10*int(num), 0, 10, 12))
+        ground.update(SPEED)
+        ground.draw(win)
+        cloud_group.update(SPEED - 3, dino)
+        cloud_group.draw(win)
+        stars_group.update(SPEED - 3, dino)
+        stars_group.draw(win)
+        cactus_group.update(SPEED, dino)
+        cactus_group.draw(win)
+        ptera_group.update(SPEED - 1, dino)
+        ptera_group.draw(win)
+        dino.update(jump, duck)
+        dino.draw(win)
 
-		if not dino.alive:
-			win.blit(game_over_img, (WIDTH//2-100, 55))
-			win.blit(replay_img, replay_rect)
+        string_score = str(score).zfill(5)
+        for i, num in enumerate(string_score):
+            win.blit(numbers_img, (520 + 11 * i, 10), (10 * int(num), 0, 10, 12))
 
-			if replay_rect.collidepoint(mouse_pos):
-				reset()
+        if high_score:
+            win.blit(numbers_img, (425, 10), (100, 0, 20, 12))
+            string_score = f'{high_score}'.zfill(5)
+            for i, num in enumerate(string_score):
+                win.blit(numbers_img, (455 + 11 * i, 10), (10 * int(num), 0, 10, 12))
 
-	pygame.draw.rect(win, WHITE, (0, 0, WIDTH, HEIGHT), 4)
-	clock.tick(FPS)
-	pygame.display.update()
-	
+        if not dino.alive:
+            win.blit(game_over_img, (WIDTH // 2 - 100, 55))
+            win.blit(replay_img, replay_rect)
+
+            if replay_rect.collidepoint(mouse_pos):
+                reset()
+
+    pygame.draw.rect(win, WHITE, (0, 0, WIDTH, HEIGHT), 4)
+    clock.tick(FPS)
+    pygame.display.update()
+
+    if record:
+        frame = pygame.surfarray.array3d(win).swapaxes(0, 1)
+        packet = av.VideoFrame.from_ndarray(frame, format='rgb24')
+        packet = video_stream.encode(packet)
+        container.mux(packet)
 
 pygame.quit()
+container.close()
